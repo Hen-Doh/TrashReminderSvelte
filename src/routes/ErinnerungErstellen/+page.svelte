2b5ac91ed5b9b9ff02709f10b212d625b0d27f94
Hen-Doh <hendoh@gmx.de>
2025-09-25 16:25:45 +0200

    onto Notifications

--- src/routes/ErinnerungErstellen/+page.svelte
+++ src/routes/ErinnerungErstellen/+page.svelte
@@ -1,11 +1,18 @@
 <script>
         import { json } from '@sveltejs/kit';
     import PocketBase from 'pocketbase';
+    import { onMount } from 'svelte';
     
     const pb = new PocketBase('http://127.0.0.1:8090');
-    let record=$state("record")
-    //async checkIfUserHasReminder().
-    async function request(){
+    let erinnerungen:Recordmodel[]=$state([])
+    async function checkIfUserHasReminder(){
+        let erinnerung = await pb.collection("Erinnerungen").getFullList()
+
+        if(erinnerung){
+
+        }
+    }
+    async function createReminder(){
             // example create data
         const data = {
             "Pickup": "2022-01-01 10:00:00.123Z",
@@ -14,11 +21,13 @@
         };
 
         record = await pb.collection('Erinnerung').create(data);
-        //console.log(record)
-        console.log(JSON.stringify(record, null, 2)); // Pretty-printed JSON output
+        //console.log(JSON.stringify(record, null, 2)); // Pretty-printed JSON output
 
     }
+    onMount(()=>{
+
+    })
 </script>
 
-<button onclick={request}>recordkeeping</button>
+<button onclick={createReminder}>recordkeeping</button>
 <h2>{record}</h2>
--- src/routes/Settings/+page.svelte
+++ src/routes/Settings/+page.svelte
@@ -6,7 +6,7 @@
 	import { ClientResponseError, type RecordModel } from 'pocketbase';
 	import { onMount } from 'svelte';
     let loggedIn = $state(pb.authStore.isValid)
-    let tempSettings = {
+    let tempSettings = $state({
         Strasse:"Hinter dem Mond",
         Hausnummer:1000,
         Relevante_Typen: [
@@ -20,7 +20,7 @@
         Erinnerungs_Vorlauf: 12,
         Benachrichtigungsart: "Email",
         user:pb.authStore.record?.id
-    } satisfies {
+    }) satisfies {
         Strasse: string;
         Hausnummer:number;
         Relevante_Typen: string[];
@@ -31,12 +31,12 @@
 
     
     let settings:RecordModel[] = $state([])
-    if(!pb.authStore.isValid){goto("/auth/login")}
+    //if(!pb.authStore.isValid){goto("/auth/login")}
     async function getSettings(){
         //nur die Settings des aktuellen user, filter via Pocketbase Regeln
         try{
             settings = await pb.collection('Settings').getFullList()
-            return settings
+            console.debug("fetched settings")
         }catch(err){
             console.log("das will ich lesen...")
             $inspect(settings).with(console.trace)
@@ -46,6 +46,7 @@
     async function createSetting(){
         try {
             await pb.collection('Settings').create(tempSettings);
+            console.debug("created Settings")
         } catch (err) {
             if (err instanceof ClientResponseError) {
                 console.error("PB create error:", err.response); // <-- shows validation details
@@ -55,23 +56,9 @@
         }
     }
     async function updateSettings() {
-        try{
-            //TODO besseres überprüfen ob es valide eingaben sind
-            let bn = (document.getElementById("Benachrichtigungsart") as HTMLSelectElement)?.value;
-            let str = (document.getElementById("Strasse") as HTMLInputElement)?.value;
-            let hn = (document.getElementById("Hausnummer") as HTMLInputElement)?.value;
-            let tm = (document.getElementById("Zeitvorlauf") as HTMLInputElement)?.value;
-            if(str=""){
-                //TODO
-            }
-            if(isNaN(hn))
-
-
-        }catch (err){
-            console.log(err)
-        }
         try {
             await pb.collection('Settings').update(settings[0].id,tempSettings);
+            console.debug("updated Settings")
         } catch (err) {
             if (err instanceof ClientResponseError) {
                 console.error("PB create error:", err.response); // <-- shows validation details
@@ -83,6 +70,7 @@
     onMount(()=>{
         if(!loggedIn){goto("/")}
         getSettings().then(()=>{
+                console.debug("settings.length:"+settings.length)
                 if(settings.length === 0){
                     createSetting()
                 }else if(settings.length === 1){
@@ -106,27 +94,25 @@
     
     //----Mülltypen-------
     let trashOptions = Object.values(trashTypes);
-    let selectedTrash: string[] = $state(tempSettings.Relevante_Typen);
-    function toggleOption(option: string) {
-        if (selectedTrash.includes(option)) {
-            selectedTrash = selectedTrash.filter(o => o !== option);
-            console.info(tempSettings.Relevante_Typen)
+    function toggleOption(option: trashTypes) {
+        if (tempSettings.Relevante_Typen.includes(option)) {
+            tempSettings.Relevante_Typen = tempSettings.Relevante_Typen.filter(o => o !== option);
         } else {
-            selectedTrash = [...selectedTrash, option];
+            tempSettings.Relevante_Typen = [...tempSettings.Relevante_Typen, option];
         }
     }
     function removeOption(option: string) {
-        selectedTrash = selectedTrash.filter(o => o !== option);
+        tempSettings.Relevante_Typen = tempSettings.Relevante_Typen.filter(o => o !== option);
     }
 </script>
 
-
+<text>{settings[0]}</text>
 <div class="max-w-xl mx-auto bg-white rounded-2xl shadow p-6 space-y-4">
     <!--Mülltypen auswahl-->
     <h2 class="text-lg font-semibold text-gray-800">Relevante Müll Typen</h2>
     <!-- Ausgewählte Mülltypen -->
         <div class="flex flex-wrap gap-2">
-            {#each selectedTrash as option}
+            {#each tempSettings.Relevante_Typen as option}
                 <div class="flex items-center bg-indigo-100 text-indigo-700 px-3 py-1 rounded-full text-sm">
                     <span>{option}</span>
                     <button
@@ -146,7 +132,7 @@
                     onclick={() => toggleOption(option)}
                     class="px-3 py-1 rounded-full border text-sm 
                         transition 
-                        {selectedTrash.includes(option) 
+                        {tempSettings.Relevante_Typen.includes(option) 
                             ? 'bg-indigo-600 text-white border-indigo-600' 
                             : 'bg-gray-100 text-gray-700 border-gray-300 hover:bg-gray-200'}"
                 >
@@ -156,21 +142,22 @@
         </div>
     <!--Benachrichtigungsart-->
     <h2 class="text-lg font-semibold text-gray-800">Benachrichtigungsart (gerade gibt es nur eine)</h2>
-    <select id="Benachrichtigungsart" class="select">
-        <option>{tempSettings.Benachrichtigungsart}</option>
+    <select bind:value={tempSettings.Benachrichtigungsart} class="select">
+        <!--TODO -->
+        <option value="Email">Email</option>
     </select>
     <!--Zeitvorlauf-->
     <h2 class="text-lg font-semibold text-gray-800">Um wie viel Uhr am Vortag möchtest du erinnert werden?</h2>
-    <input id="Zeitvorlauf" type="time" class="input" placeholder="1200"/>
+    <input bind:value={tempSettings.Erinnerungs_Vorlauf} type="time" class="input" placeholder="1200"/>
     <!--Adressauswahl TODO via Karte falls Zeit bleibt 
     TODO überprüfen ob die Adresse Valide ist
     -->
     <div>
         <h2 class="text-lg font-semibold text-gray-800">Adresse</h2>
         <h3 class="text-base font-semibold text-gray-800">Straße</h3>
-        <input id="Strasse" type="text" placeholder={tempSettings.Strasse} class="input" />
+        <input bind:value={tempSettings.Strasse} type="text" placeholder={tempSettings.Strasse} class="input" />
         <h3 class="text-base font-semibold text-gray-800">Hausnummer</h3>
-        <input id="Hausnummer" type="text" placeholder={tempSettings.Hausnummer.toString()} class="input" />
+        <input bind:value={tempSettings.Hausnummer} type="text" placeholder={tempSettings.Hausnummer.toString()} class="input" />
         <div></div>
     </div>
     <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl" onclick={updateSettings} >Speichern</button>
